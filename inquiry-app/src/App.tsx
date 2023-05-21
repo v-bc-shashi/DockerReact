9181531287c5b8d716d4e16f694f6654b295db72
Shashi Prasad <v-bc-shashi@users.noreply.github.com>
2023-05-21 21:37:16 +0530

    Code of React app Ruining

---
+++ inquiry-app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h2>Welcome to Hyderabad</h2>
+             
+          <h3>DON'T Learn React</h3>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
